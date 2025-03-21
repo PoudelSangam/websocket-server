537bcc783ddcde2de8a5006cc22ff5aa2760b40b
= <=>
2025-03-21 09:59:29 +0545

    first commit

--- server.js
+++ server.js
@@ -65,3 +65,5 @@ wss.on("connection", (ws) => {
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// Serve static files from the public directory
